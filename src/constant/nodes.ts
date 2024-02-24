6169215dc21c82698c6dbfd0bdb4d23915554123
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-02-24 18:58:05 +0200

    feat: create obj with all nodes and theme mode const

---
+++ src/constant/nodes.ts
@@ -0,0 +1,16 @@
+export const NODES = {
+  buttonLeast: document.getElementById("smallest") as HTMLButtonElement,
+  buttonBiggest: document.getElementById("biggest") as HTMLButtonElement,
+  buttonMedian: document.getElementById("median") as HTMLButtonElement,
+  buttonMean: document.getElementById("mean") as HTMLButtonElement,
+  resultDiv: document.getElementById("result") as HTMLDivElement,
+  input: document.getElementById("input_load_file") as HTMLInputElement,
+  inputArea: document.querySelector(".input_area") as HTMLDivElement,
+  themeToggleButton: document.querySelector(
+    ".toggle-theme"
+  ) as HTMLButtonElement,
+  accordionBtn: document.querySelector(".accordion-btn") as HTMLButtonElement,
+  accordionContent: document.querySelector(
+    ".accordion-content"
+  ) as HTMLDivElement,
+};
---
+++ src/constant/theme-mode.ts
@@ -0,0 +1,3 @@
+export const THEME_MODE = {
+  mode: localStorage.getItem("theme-mode"),
+};
