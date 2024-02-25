34b68bbbe6c241fe55c6e6dfa3470752739e4ad5
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-02-25 22:54:47 +0200

    refactor: separate logic

--- src/main.ts
+++ src/main.ts
@@ -1,72 +1,25 @@
-import { NODES } from "./constant/nodes";
 import { THEME_MODE } from "./constant/theme-mode";
-import { handlerEvents } from "./utils/handler-events";
-import { readFileContents } from "./utils/read-file-contents";
-import { renderAlertMessage } from "./utils/render-alert-message";
-import { disableDarkMode, enableDarkMode } from "./utils/handle-change-theme";
+import { toggleTheme } from "./utils/toggle-theme";
+import { enableDarkMode } from "./utils/enable-dark-mode";
+import { handleLoadFile } from "./utils/handle-load-file";
+import { toggleAccordion } from "./utils/toggle-accordion";
 
 import "./style.css";
 
-const { mode } = THEME_MODE;
-const { input, themeToggleButton, accordionBtn, accordionContent } = NODES;
+const accordionBtn = document.querySelector(
+  ".accordion-btn"
+) as HTMLButtonElement;
+const themeToggleButton = document.querySelector(
+  ".toggle-theme"
+) as HTMLButtonElement;
+const inputLoadFile = document.getElementById(
+  "input_load_file"
+) as HTMLInputElement;
 
-themeToggleButton.addEventListener("click", () => {
-  const mode = localStorage.getItem("theme-mode");
-
-  if (mode !== "dark") {
-    enableDarkMode();
-  } else {
-    disableDarkMode();
-  }
-});
-
-if (mode === "dark") {
+if (THEME_MODE === "dark") {
   enableDarkMode();
 }
 
-input.addEventListener("change", function (e) {
-  const file = (e.target as HTMLInputElement).files?.[0];
-
-  if (!file) {
-    return;
-  }
-
-  readFileContents(file)
-    .then((array) => {
-      if (array.length === 1 && array[0] === 0) {
-        renderAlertMessage({
-          message: "File is empty",
-          status: "error",
-        });
-        return array;
-      } else {
-        renderAlertMessage({
-          message: "File read successfully",
-          status: "success",
-        });
-      }
-      return array;
-    })
-    .then((numbersArray) => {
-      handlerEvents(numbersArray);
-    })
-    .catch((error) => {
-      console.error("Error reading file:", error);
-
-      renderAlertMessage({
-        message: "Error reading file",
-        status: "error",
-      });
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  accordionBtn.addEventListener("click", function () {
-    accordionContent.classList.toggle("show");
-    if (accordionContent.classList.contains("show")) {
-      accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
-    } else {
-      accordionContent.style.maxHeight = "0";
-    }
-  });
-});
+themeToggleButton.addEventListener("click", toggleTheme);
+inputLoadFile.addEventListener("change", handleLoadFile);
+accordionBtn.addEventListener("click", toggleAccordion);
