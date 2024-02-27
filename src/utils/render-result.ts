96522a3cc05208261a4c8ff6aa10de18f637a923
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-02-27 21:57:33 +0200

    chore: update import and type

--- src/utils/render-result.ts
+++ src/utils/render-result.ts
@@ -1,11 +1,15 @@
 export const renderResult = (result: number | number[]): void => {
-  const resultWindow = document.getElementById("result") as HTMLDivElement;
+  const resultWindow = document.getElementById("result");
+
+  if (!resultWindow) return;
 
   if (Array.isArray(result)) {
     resultWindow.textContent = `Result: ${result}`;
     resultWindow.style.fontSize = "14px";
+
     return;
   }
 
-  resultWindow.textContent = `Result: ${result.toString()}`;
+  resultWindow.style.fontSize = "30px";
+  resultWindow.textContent = `Result: ${result}`;
 };
