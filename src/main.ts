c88bf7b4c7fe707b8bf1c2cec1e24627ba302d21
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-02-27 21:54:25 +0200

    refactor: separate events logic

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,11 @@
+import { initEvents } from "./utils/init-events";
 import { THEME_MODE } from "./constant/theme-mode";
-import { toggleTheme } from "./utils/toggle-theme";
-import { enableDarkMode } from "./utils/enable-dark-mode";
-import { handleLoadFile } from "./utils/handle-load-file";
-import { toggleAccordion } from "./utils/toggle-accordion";
+import { toggleDarkMode } from "./utils/toggle-dark-mode";
 
 import "./style.css";
 
-const accordionBtn = document.querySelector(
-  ".accordion-btn"
-) as HTMLButtonElement;
-const themeToggleButton = document.querySelector(
-  ".toggle-theme"
-) as HTMLButtonElement;
-const inputLoadFile = document.getElementById(
-  "input_load_file"
-) as HTMLInputElement;
-
 if (THEME_MODE === "dark") {
-  enableDarkMode();
+  toggleDarkMode("dark");
 }
 
-themeToggleButton.addEventListener("click", toggleTheme);
-inputLoadFile.addEventListener("change", handleLoadFile);
-accordionBtn.addEventListener("click", toggleAccordion);
+initEvents();
