d7909c71fc67dbb55e6a58b27f9a1e8f3ce91832
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-02-27 21:56:42 +0200

    chore: separate array with functions

---
+++ src/constant/array-count-functions.ts
@@ -0,0 +1,15 @@
+import { findBiggestNumber } from "../utils/find-biggest-number";
+import { findLargestDecreasingSequence } from "../utils/find-largest-decreasing-sequence";
+import { findLargestIncreasingSequence } from "../utils/find-largest-increasing-sequence";
+import { findLeastNumber } from "../utils/find-least-number";
+import { findMean } from "../utils/find-mean";
+import { findMedian } from "../utils/find-median";
+
+export const arrayCountFunctions = [
+  findLeastNumber,
+  findBiggestNumber,
+  findMedian,
+  findMean,
+  findLargestIncreasingSequence,
+  findLargestDecreasingSequence,
+];
